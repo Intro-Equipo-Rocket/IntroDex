74cf889e04c2c3bec5f5f06849956bca848c4fef
Diego Fernández <diego.j.fernandez.g@gmail.com>
2024-10-09 23:06:49 -0300

    Eliminacion de carpetas/archivos sin uso tras la reestructuracion de la app.

--- cruds/pokemon_id_crud.py
+++
@@ -1,4 +0,0 @@
-from pokemon_id import Pokemon
-from fastapi import HTTPException, status, APIRouter
-
-router = APIRouter(prefix="/pokemon_id")
--- pokemon_id.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Pokemon(BaseModel):
-    id: int
