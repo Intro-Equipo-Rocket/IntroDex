dd6511785ff367a65081b5b490e4ca5af6a0ecbd
Diego Fernández <diego.j.fernandez.g@gmail.com>
2024-10-14 20:27:07 -0300

    Arreglando conflictos.

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ app.include_router(api_router_get_pokemon_moves_id)
 
 @app.get("/")
 def root():
-    return {"message": "Bienvenido a la API de pokemon del Equipo Rocket!"}
+    return {"mensaje": "Bienvenido a la API de pokemon del Equipo Rocket!"}
---
+++ app/routers/movimientos.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from app.modelos import *
+from app.db import *
+
+router = APIRouter()
+
+@router.get('/{movimiento_id}')
+def obtener_movimiento_por_id(movimiento_id: int):
+    pass
+
+@router.get('/{movimiento_id}/pokemons')
+def obtener_pokemons_por_movimiento(movimiento_id: int):
+    pass
