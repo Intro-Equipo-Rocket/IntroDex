667da9b1678b1b38ba60c50ab4aedab928ad61b9
nicsv04 <ndursi@fi.uba.ar>
2024-10-14 15:27:48 -0300

    Agrego achivos __init__.py en las carpetas

--- app/tests/test_GET_pokemons.py
+++ app/tests/test_GET_pokemons.py
@@ -1,10 +1,10 @@
-from main import app
+from app.main import app
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
 
 def test_obtener_pokemones():
-    response = client.get("/")
+    response = client.get("/pokemon")
     assert response.status_code == 200
     assert len(response.json()) == 5
