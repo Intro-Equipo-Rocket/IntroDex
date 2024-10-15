fd62d670d1a21ce3c9e65c1ac3aace8d9e7432f0
Luis Stella <luisstella.m@gmail.com>
2024-10-15 12:59:43 -0300

    Primer Intento de test

---
+++ app/tests/routers_delete_teams_id/test_delete_team.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from app.main import app
+from db.equipos_db import equipos_db
+
+client_test = TestClient(app)
+
+def test_eliminar_equipo_existente():
+    equipo_id = 1
+    pagina = client_test.delete(f'/equipos/{equipo_id}')
+
+    assert pagina.status_code == 200
+    assert pagina.json() == {'mensaje': f'El equipo (Equipo 1) con id ({equipo_id}) ha sido eliminado.'}
+    assert not any(equipo['id'] == equipo_id for equipo in equipos_db)
+
+def test_eliminar_equipo_inexistente():
+    equipo_id = 55
+    pagina = client_test.delete(f'/equipos/{equipo_id}')
+
+    assert pagina.status_code == 404
+    assert pagina.json() == {'detail': f'No se ha encontrado al equipo con id ({equipo_id}).'}
