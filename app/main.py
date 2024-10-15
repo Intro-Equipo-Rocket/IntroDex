38f743b097103f0b3dadd0ccd139df0598dba212
Luis Daniel <lcifuentes@fi.uba.ar>
2024-10-15 17:25:23 -0300

    Agregado el router de naturalezas al main principal

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 app.include_router(pokemon.router, prefix='/pokemons', tags=['Pokemons'])
 app.include_router(movimientos.router, prefix='/movimientos', tags=['Movimientos'])
 app.include_router(equipos.router, prefix='/equipos', tags=['Equipos'])
+app.include_router(naturalezas.router, prefix="/naturalezas", tags=["Naturaleza"])
 
 @app.get('/')
 def root():
