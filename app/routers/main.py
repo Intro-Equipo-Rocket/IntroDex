6d048a9f07995ca514e33e122ea02fec65e63cb0
Dursi Nicolas Elias <ndursi@fi.uba.ar>
2024-10-15 22:57:01 -0300

    Borro main.py de la carpeta routers

--- app/routers/main.py
+++ app/routers/main.py
@@ -1,8 +1,1 @@
-from fastapi import APIRouter
 
-from app.routers import pokemones
-
-api_router_get_pokemones = APIRouter()
-api_router_get_pokemones.include_router(
-    pokemones.router, prefix="/pokemon", tags=["Pokemon"]
-)
