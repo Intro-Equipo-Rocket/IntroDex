11aae3f2164c18b70e063c9dc4e1f9fe702b2436
nicsv04 <ndursi@fi.uba.ar>
2024-10-15 23:02:24 -0300

    Borro main.py en carpeta routers

--- app/routers/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.routers import post_pokemon
-
-api_router_post_pokemon = APIRouter()
-api_router_post_pokemon.include_router(
-    post_pokemon.router, prefix="/pokemon", tags=["Pokemon"]
-)
