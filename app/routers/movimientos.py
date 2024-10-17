5c5ebc22df8e6b213291ede094d0203d07a08f61
Luis Daniel <lcifuentes@fi.uba.ar>
2024-10-16 22:20:35 -0300

    Arreglado en routers la importacion del modelo de movimiento

--- app/routers/movimientos.py
+++ app/routers/movimientos.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, HTTPException, status
-from app.modelos import Movimiento, Error
+from app.modelos import Movimientos, Error
 from app.db.movimientos_db import *
 
 router = APIRouter()
 
 
 @router.get("/id/{id}", responses={status.HTTP_404_NOT_FOUND: {"model": Error}})
-def get_pokemon(id: int) -> Movimiento:
+def get_pokemon(id: int) -> Movimientos:
     for move in Moves:
         if move.id == id:
             move.pokemones_aprenden_evolucionar = None
@@ -20,7 +20,7 @@ def get_pokemon(id: int) -> Movimiento:
 
 
 @router.get("/nombre/{nombre}", responses={status.HTTP_404_NOT_FOUND: {"model": Error}})
-def get_pokemon(nombre: str) -> Movimiento:
+def get_pokemon(nombre: str) -> Movimientos:
     for move in Moves:
         if move.nombre.lower() == nombre.lower():
             move.pokemones_aprenden_evolucionar = None
