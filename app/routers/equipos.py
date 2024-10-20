c9d00853e84cb99aa20987211810c976079360a5
Luis Stella <luisstella.m@gmail.com>
2024-10-20 09:01:38 -0300

    Implementación del endpoint

--- app/routers/equipos.py
+++ app/routers/equipos.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.modelos import *
 from app.db.equipos_db import *
+from typing import List
 
 router = APIRouter()
 
@@ -10,9 +11,18 @@ def obtener_naruralezas():
     pass
 
 
-@router.get("/")
-def obtener_equipos():
-    pass
+@router.get("/", response_model=List[Equipo])
+def obtener_equipos(pagina: int, cantidad_pokemons: int = 10):
+    if not pagina >= 1 or not cantidad_pokemons >= 0:
+        raise HTTPException(status_code=400, detail="Algunos de los parámetros están siendo mal introducidas")
+    
+    skip = (pagina - 1) * 10
+    equipos_pagina = equipos_db[skip:skip + cantidad_pokemons]
+
+    if not equipos_pagina:
+        raise HTTPException(status_code=404, detail="No se encontraron equipos para esta página")
+    
+    return equipos_pagina
 
 
 @router.post("/")
