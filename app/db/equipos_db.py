f7eaa481222c3158f486da7bd6d39c96dfb98308
Luis Daniel <lcifuentes@fi.uba.ar>
2024-10-20 21:14:30 -0300

    Agregados algunos datos temporales al database

---
+++ app/db/equipos_db.py
@@ -0,0 +1,16 @@
+from app.modelos import Equipo, IntegranteEquipo
+
+equipos: list[Equipo] = [
+    Equipo(
+        id=22,
+        nombre="Equipo 1",
+        pokemones=[],
+        generacion=0
+    ),
+    Equipo(
+        id=27,
+        nombre="DREAM TEAM",
+        pokemones=[],
+        generacion=1
+    ),
+]
