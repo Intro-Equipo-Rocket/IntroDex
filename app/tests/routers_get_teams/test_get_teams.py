c94539b1327deec6461ce45d9ae146c78accf858
Lilstella <155794304+Lilstella@users.noreply.github.com>
2024-10-21 04:33:41 -0300

    Areglando la ruta de los tests

--- app/tests/routers_get_teams/test_get_teams.py
+++ app/tests/routers_get_teams/test_get_teams.py
@@ -5,24 +5,24 @@ test = TestClient(app)
 
 def test_obtener_equipos_pagina_1():
     pagina = 1
-    test_1 = test.get(f"/equipos/?pagina={pagina}&cantidad_pokemons=10")
+    test_1 = test.get(f"/equipos/pagina/{pagina}?pagina={pagina}&cantidad_pokemons=10")
     assert test_1.status_code == 200
     assert len(test_1.json()) == 10
 
 def test_obtener_equipos_pagina_sin_equipos():
     pagina = 4
-    test_2 = test.get(f"/equipos/?pagina={pagina}&cantidad_pokemons=10")
+    test_2 = test.get(f"/equipos/pagina/{pagina}?pagina={pagina}&cantidad_pokemons=10")
     assert test_2.status_code == 404
     assert test_2.json() == {'detail': 'No se encontraron equipos para esta página'} 
 
 def test_pagina_invalida():
     pagina = 0
-    test_3 = test.get(f"/equipos/?pagina={pagina}&cantidad_pokemons=10")
+    test_3 = test.get(f"/equipos/pagina/{pagina}?pagina={pagina}&cantidad_pokemons=10")
     assert test_3.status_code == 404
     assert test_3.json() == {'detail': 'Algunos de los parámetros están siendo mal introducidas'}
 
 def test_cantidad_olvidada():
     cantidad = 0
-    test_4 = test.get(f"/equipos/?pagina=1&cantidad_pokemons={cantidad}")
+    test_4 = test.get(f"/equipos/pagina/{pagina}?pagina=1&cantidad_pokemons={cantidad}")
     assert test_4.status_code == 404
     assert test_4.json() == {'detail': 'Algunos de los parámetros están siendo mal introducidas'}
