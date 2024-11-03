dbbdd13e510a362284de23901f40fd44d6da3e25
Luis Daniel <lcifuentes@fi.uba.ar>
2024-11-03 16:27:01 -0300

    Implementado una suposicion de lo que seria el endpoint

--- app/routers/naturalezas.py
+++ app/routers/naturalezas.py
@@ -1,11 +1,21 @@
 from fastapi import APIRouter, HTTPException, status
 from app.modelos import Naturaleza
 from app.db.naturaleza_db import naturalezas
+# from sqlmodel import Session, select
+# from app.database import SessionDep
 
 router = APIRouter()
 
 @router.get("/", response_model=list[Naturaleza])
-def obtener_naturalezas() -> list[Naturaleza]:
+def obtener_naturalezas() -> list[Naturaleza]:  # def obtener_naturalezas(session: Session = SessionDep):
+    # query = select(Naturaleza)
+    # naturalezas = session.exec(query).all()
+    # if not naturalezas:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND,
+    #         detail="Naturalezas no encontradas"
+    #     )
+    # return naturalezas
     if not naturalezas:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
