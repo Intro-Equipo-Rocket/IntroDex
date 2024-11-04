2212ff79d85416dd5f9049438c07223901d36da0
nicsv04 <ndursi@fi.uba.ar>
2024-11-04 14:09:43 -0300

    Tabla de naturaleza creada

---
+++ app/alembic/versions/tabla_naturaleza/2024_11_04_0201-95e3f1435596_crear_tabla_naturaleza.py
@@ -0,0 +1,36 @@
+"""Crear tabla naturaleza
+
+Revision ID: 95e3f1435596
+Revises: 
+Create Date: 2024-11-04 02:01:59.396941
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "95e3f1435596"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "naturaleza",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("identifier", sa.Text, nullable=False),
+        sa.Column("decreased_stat_id", sa.Integer, nullable=False),
+        sa.Column("increased_stat_id", sa.Integer, nullable=False),
+        sa.Column("hates_flavor_id", sa.Integer, nullable=False),
+        sa.Column("likes_flavor_id", sa.Integer, nullable=False),
+        sa.Column("game_index", sa.Integer, nullable=False),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("naturaleza")
