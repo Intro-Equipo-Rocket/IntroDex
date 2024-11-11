71e0ab41203c56ee578d7665134b03d1665a9f6e
Diego Fernández <diego.j.fernandez.g@gmail.com>
2024-11-11 11:29:59 -0300

    mejoras de los test.

--- app/tests/test_get_move_id.py
+++ app/tests/test_get_move_id.py
@@ -22,3 +22,13 @@ def test_no_existe_movimiento() -> None:
 
     response = client.get("/movimientos/nombre/dadafgadasdafasd")
     assert response.status_code == 404
+
+
+def test_verificar_datos_por_movimiento_id_existente() -> None:
+    response = client.get("/movimientos/id/144")
+    assert response.json()["nombre"] == "Transformación"
+
+
+def test_verificar_datos_por_nombre_existente() -> None:
+    response = client.get("/movimientos/nombre/Transformación")
+    assert response.json()["id"] == 144
